6cd9dad47f7ed9aedf2dd7097ba6cbfa5b6bf925
unilogue <unilogue.io@gmail.com>
2018-02-24 20:22:35 -0500

    fixed bg

--- js/index.js
+++ js/index.js
@@ -9,17 +9,17 @@ $(document).ready(function() {
   });
 
   function backgroundSequence() {
-	window.clearTimeout();
-	var k = 0;
-	for (i = 0; i < bgImageArray.length; i++) {
-		setTimeout(function(){
-			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
-			document.documentElement.style.backgroundSize ="cover";
-		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 2000))} else { k++; }
-		}, (secs * 1000) * i)
-	}
-}
-backgroundSequence();
+  	window.clearTimeout();
+  	var k = 0;
+  	for (i = 0; i < bgImageArray.length; i++) {
+  		setTimeout(function(){
+  			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+  			document.documentElement.style.backgroundSize ="cover";
+  		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }
+  		}, (secs * 1000) * i)
+  	}
+  }
+  backgroundSequence();
 
 
 
