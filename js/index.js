5fc334edc2007a22dccd750d1615362551cb8c1f
unilogue <unilogue.io@gmail.com>
2018-03-10 03:42:49 -0500

    added about

--- js/index.js
+++ js/index.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+  $(function(){
+      $(window).resize(function(e){
+          placeFooter();
+      });
+      placeFooter();
+      // hide it before it's positioned
+      $('#footer').css('display','inline');
+  });
+
+  function placeFooter() {
+      var windHeight = $(window).height();
+      var footerHeight = $('#footer').height();
+      var offset = parseInt(windHeight) - parseInt(footerHeight);
+      $('#footer').css('top',offset);
+  }
+
+
+  var $allContentDivs = $('.navContent div') // Hide All Content Divs
+
+$('#navbar a').click(function(){
+    var $contentDiv = $("#" + this.id + "content");
+
+    if($contentDiv.is(":visible")) {
+        $contentDiv.fadeOut(); // Hide Div
+    } else {
+        $allContentDivs.fadeOut(); // Hide All Divs
+        $contentDiv.fadeIn(); // Show Div
+    }
+
+    return false;
+  });
+
   var images = [
     "images/bg1.jpg",
     "images/bg2.jpg",
@@ -7,6 +39,7 @@ $(document).ready(function() {
     "images/bg4.jpg",
     "images/bg5.jpg",
     "images/bg6.jpg",
+    "images/bg7.jpg"
   ];
   var $body = $("body"),
       $bg = $("#bg"),
@@ -29,6 +62,18 @@ $(document).ready(function() {
   }());
 
 
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
   $('#fullpage').fullpage({
     //Navigation
@@ -55,7 +100,7 @@ $(document).ready(function() {
     loopHorizontal: true,
     continuousVertical: false,
     continuousHorizontal: false,
-    scrollHorizontally: false,
+    scrollHorizontally: true,
     interlockedSlides: false,
     dragAndMove: true,
     offsetSections: false,
